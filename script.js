3c11a68821f2f4b807ced8402dbc8ac33c06ac28
Balaji <131993048+Balajiruse@users.noreply.github.com>
2023-09-29 16:47:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,55 @@
+const times=document.getElementById("time")
+let display = 10;
+setTimeout(()=>{
+    times.innerText=display--
+
+    setTimeout(()=>{
+        times.innerText=display--
+
+    setTimeout(()=>{
+        times.innerText=display--
+
+setTimeout(()=>{
+    times.innerText=display--
+
+    setTimeout(()=>
+    {
+        times.innerText=display--
+
+        setTimeout(()=>{
+            times.innerText=display--
+
+            setTimeout(()=>{
+                times.innerText=display--
+
+                setTimeout(()=>{
+                    times.innerText=display--
+
+
+                    setTimeout(()=>{
+                        times.innerText=display--
+
+                        setTimeout(()=>{
+                            times.innerText=display--
+
+                            setTimeout(()=>
+                            {
+                                times.innerText=display--
+
+                               setTimeout(()=>{
+                                times.innerText="Happy new Year 2024"
+
+
+                               },1000) 
+                            },1000)
+                        },1000)
+                    
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+  },1000)
+  },1000)   
+  },1000)
+},1000)
